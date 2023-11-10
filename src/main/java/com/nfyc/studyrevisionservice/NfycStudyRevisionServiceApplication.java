eba3dd92b49d9033f2fb746e52da78c28c695984
Aditya SHUKLA <aditya.shukla@outpayce.com>
2023-11-11 01:44:01 +0530

    intial project setup

---
+++ src/main/java/com/nfyc/studyrevisionservice/NfycStudyRevisionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nfyc.studyrevisionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfycStudyRevisionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfycStudyRevisionServiceApplication.class, args);
+    }
+
+}
