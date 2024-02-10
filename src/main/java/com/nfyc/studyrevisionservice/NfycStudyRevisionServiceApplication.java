a5abab642641ba0bea5dfc5a554f78a67ead3aa4
RoopaRavindranath <roopar@fico.com>
2024-02-10 09:22:06 +0530

    Eureka client setup

--- src/main/java/com/nfyc/studyrevisionservice/NfycStudyRevisionServiceApplication.java
+++ src/main/java/com/nfyc/studyrevisionservice/NfycStudyRevisionServiceApplication.java
@@ -2,8 +2,10 @@ package com.nfyc.studyrevisionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NfycStudyRevisionServiceApplication {
 
     public static void main(String[] args) {
